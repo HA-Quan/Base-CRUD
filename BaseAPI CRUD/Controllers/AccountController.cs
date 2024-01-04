65bb32f18f70d4d442e58854d82d444fb41842ee
nguyen.vu <nguyen.vu@kiaisoft.com>
2024-01-04 10:27:06 +0700

    test 2

--- BaseAPI CRUD/Controllers/AccountController.cs
+++ BaseAPI CRUD/Controllers/AccountController.cs
@@ -37,7 +37,7 @@ namespace BaseAPI_CRUD.Controllers
                 return BadRequest(new ApiReponse
                 {
                     Success = false,
-                    Message = "Something is wrongs test conflict!"
+                    Message = "Something is wrongs test conflict 2!"
                 });
             }
         }
